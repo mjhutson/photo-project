fb19c782759644fd963da0989802529ad14533cf
mjhutson <hutson.matthew.j@gmail.com>
2022-05-07 11:06:56 -0500

    Update Startup.cs

--- photo-project/photo-project/Startup.cs
+++ photo-project/photo-project/Startup.cs
@@ -3,13 +3,41 @@ namespace photo_project
 {
     public class Startup
     {
+        private readonly ContainerBuilder _containerBuilder;
+        private readonly Container _container;
+
         public Startup()
         {
+            _containerBuilder = new ContainerBuilder();
+            SetupContainer(_containerBuilder);
+            _container = _builder.Build();
         }
 
-        internal object GetContainer()
+        public void SetupContainer(ContainerBuilder builder)
         {
-            throw new NotImplementedException();
+            builder.RegisterType<AlbumJobs>().As<IAlbumJobs>();
+            builder.RegisterType<AlbumService>().As<IAlbumService>();
+            builder.RegisterType<AlbumController>().As<IAlbumController>();
+            builder.RegisterType<UserInputService>().As<IUserInputService>();
+            builder.RegisterType<HttpClientWrapper>().As<IHttpClientWrapper>();
+            builder.Register(c => new HttpClient()
+            {
+                BaseAddress = new Uri(GetConfiguration()["AlbumApi:Endpoint"])
+            })
+            .Named<HttpClient>(GetConfiguration()["AlbumApi:ClientName"])
+            .SingleInstance();
+        }
+
+        public IConfigurationRoot GetConfiguration()
+        {
+            return new ConfigurationBuilder()
+                .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+                .AddJsonFile("appsettings.json")
+                .Build();
+        }
+
+        public Container GetContainer(){
+            return _container;
         }
     }
 }
