079bf8cc413171b8b3f5c9d8562059f11be24038
mjhutson <hutson.matthew.j@gmail.com>
2022-05-07 09:30:50 -0500

    input

---
+++ photo-project/photo-project-services/UserInputService.cs
@@ -0,0 +1,46 @@
+﻿using photo_project_services.Wrappers;
+
+namespace photo_project_services
+{
+    public interface IUserInputService
+    {
+        public int GetAlbumIdFromUser();
+    }
+
+    public class UserInputService : IUserInputService
+    {
+        // Constrining the user to 3 retries to prevent the potential for inf. loops
+        private const int MaxRetries = 3;
+
+        private readonly IConsoleWrapper _consoleWrapper;
+
+        public UserInputService(IConsoleWrapper consoleWrapper)
+        {
+            _consoleWrapper = consoleWrapper;
+        }
+
+        public int GetAlbumIdFromUser()
+        {
+            var userHasEnteredValidAlbumId = false;
+            var retries = 0;
+
+            _consoleWrapper.PromptForInput();
+
+            do
+            {
+                if (int.TryParse(_consoleWrapper.ReadLine(), out var id))
+                {
+                    _consoleWrapper.DisplayId(id);
+                    return id;
+                }
+                else
+                {
+                    retries++;
+                    _consoleWrapper.PromptForValidInput();
+                }
+            } while (!userHasEnteredValidAlbumId || retries < MaxRetries);
+
+            return -1;
+        }
+    }
+}
---
+++ photo-project/photo-project-tests/service-tests/UserInputTests.cs
@@ -0,0 +1,95 @@
+﻿using AutoFixture;
+using FluentAssertions;
+using NSubstitute;
+using photo_project_services;
+using photo_project_services.Wrappers;
+using Xunit;
+
+namespace photo_project_tests.servicetests
+{
+    public class UserInputTests
+    {
+        private const int FallThroughId = -1;
+
+        private readonly IConsoleWrapper _consoleWrapper;
+
+        private readonly UserInputService _sut;
+
+        private readonly int _expectedId;
+
+        public UserInputTests()
+        {
+            var fixture = new Fixture();
+
+            _expectedId = fixture.Create<int>();
+
+            _consoleWrapper = Substitute.For<ConsoleWrapper>();
+            _consoleWrapper.ReadLine().Returns(_expectedId.ToString());
+
+            _sut = new UserInputService(_consoleWrapper);
+        }
+
+
+        [Fact]
+        public void ShouldCallPromptForInputOnce()
+        {
+            _sut.GetAlbumIdFromUser();
+
+            _consoleWrapper.Received(1).PromptForInput();
+        }
+
+        [Fact]
+        public void ShouldCallDisplayIdOnceForValidInput()
+        {
+            _sut.GetAlbumIdFromUser();
+
+            _consoleWrapper.Received(1).DisplayId(Arg.Any<int>());
+        }
+
+        [Fact]
+        public void ShouldNotCallPromptForValidInputForValidInput()
+        {
+            _sut.GetAlbumIdFromUser();
+
+            _consoleWrapper.DidNotReceive().PromptForValidInput();
+        }
+
+        [Fact]
+        public void ShouldReturnExpectedIdForValidInput()
+        {
+            _sut.GetAlbumIdFromUser().Should().Be(_expectedId);
+        }
+
+        [Fact]
+        public void ShouldCallPromptForValidInputForInvalidInput()
+        {
+
+            _consoleWrapper.ReadLine().Returns("Not a number");
+
+            _sut.GetAlbumIdFromUser();
+
+            _consoleWrapper.Received(3).PromptForValidInput();
+        }
+
+        [Fact]
+        public void ShouldNotCallPDisplayIdForInvalidInput()
+        {
+
+            _consoleWrapper.ReadLine().Returns("Not a number");
+
+            _sut.GetAlbumIdFromUser();
+
+            _consoleWrapper.DidNotReceive().DisplayId(Arg.Any<int>());
+        }
+
+
+        [Fact]
+        public void ShouldReturnFallThroughIdForInvalidInput()
+        {
+
+            _consoleWrapper.ReadLine().Returns("Not a number");
+
+            _sut.GetAlbumIdFromUser().Should().Be(FallThroughId);
+        }
+    }
+}
